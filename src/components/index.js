c197230eacb09a104b8b3de760c41dacd632ee62
Arpeet Satra <arpeet17@gmail.com>
2023-01-12 19:36:00 +0530

    test commit badme rollback karna

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Login } from './Login.jsx';
 export { default as Navbar } from './Navbar.jsx';
 export { default as Cart } from './Cart.jsx';
 export { default as Hero } from './Hero.jsx';
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import App1 from './App1'
 import './index.css'
 import { Provider } from "react-redux";
 import Store from './app/Store.js';
@@ -11,6 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={Store}>
     <Toaster position='top-center' reverseOrder={false} />
       <App/>
+     
+      
     </Provider>
   </React.StrictMode>
 )
